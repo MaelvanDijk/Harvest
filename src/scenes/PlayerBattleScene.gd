0d891f237472fd734e4507bfca057cce17b9a349
Mael van Dijk <maelvandijk95@gmail.com>
2024-05-01 19:26:09 +0200

    added basic turn state machine

--- src/scenes/PlayerBattleScene.gd
+++ src/scenes/PlayerBattleScene.gd
@@ -1,26 +1,16 @@
 extends Node
 
 @onready var health_bar = $HealthComponent/BoxContainer/healthBar
-@export var attack := 10
-
-signal attack_player(damage)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	attack_player.connect(_deal_damage)
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func play_turn():
-	_deal_damage()
-	
+
 func _receive_damage(amount:int):
 	"""Handles all damage calls"""
 	health_bar.value -= amount
-	
-func _deal_damage():
-	print("enemy attacking!")
-	attack_player.emit(attack)
-	
--- src/scripts/battle_scene.gd
+++ src/scripts/battle_scene.gd
@@ -1,18 +1,35 @@
 extends Node2D
 
-@onready var EnemyBattleScene = $EnemyBattleScene
+enum turn_order {PLAYER, ENEMY}
+var turn = turn_order.PLAYER
+
+@onready var enemy_battle_scene = $EnemyBattleScene
+@onready var enemy = $EnemyBattleScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func _end_turn():
+	"""Handles turn management"""
+	match turn:
+		turn_order.PLAYER:
+			print("Switching to enemy turn")
+			turn = turn_order.ENEMY
+			_play_enemy_turn()
+		turn_order.ENEMY:
+			print("Switching to player turn")
+			turn = turn_order.PLAYER 
+			
+func _play_enemy_turn():
+	enemy.play_turn()
 
 func _on_attack_button_pressed():
 	"""Handles the attack button signal to spend resources and deal damage"""
 	print("attack!")
-	EnemyBattleScene._receive_damage(10)
+	enemy_battle_scene._receive_damage(10)
+	_end_turn()
